7d4207d219f8229d21115f761746926d52482351
Noah <noahbechtel@gmail.com>
2022-09-08 21:19:56 -0500

    Shit

--- server/db/db.js
+++ server/db/db.js
@@ -12,7 +12,7 @@ const db = new Sequelize(process.env.DATABASE_URL, {//new Sequelize(process.env.
     }
   }
 );
-exports= db
+module.exports= db
 
 // This is a global Mocha hook used for resource cleanup.
 // Otherwise, Mocha v4+ does not exit after tests.
