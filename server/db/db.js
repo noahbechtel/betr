d0cfa1e4c42ed9b760ea8dea6baf5517d1f4bd0c
Noah <noahbechtel@gmail.com>
2022-09-08 21:28:48 -0500

    hello

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
-const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
 const db = new Sequelize(process.env.DATABASE_URL, {//new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
@@ -11,7 +10,7 @@ const db = new Sequelize(process.env.DATABASE_URL, {//new Sequelize(process.env.
       }
     }
   }
-);
+)
 module.exports= db
 
 // This is a global Mocha hook used for resource cleanup.
