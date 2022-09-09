495be043d280f1d3a4a35602e056c58ad5b11a9b
Noah <noahbechtel@gmail.com>
2022-09-08 21:17:14 -0500

    Shit

--- server/db/db.js
+++ server/db/db.js
@@ -3,7 +3,7 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
-const db = new Sequelize(process.env.DATABASE_URL, {//|| `postgres://localhost:5432/${databaseName}`
+const db = new Sequelize(process.env.DATABASE_URL, {//new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
       ssl: {
         require: true,
@@ -11,8 +11,8 @@ const db = new Sequelize(process.env.DATABASE_URL, {//|| `postgres://localhost:5
       }
     }
   }
-)
-module.exports = db
+);
+exports= db
 
 // This is a global Mocha hook used for resource cleanup.
 // Otherwise, Mocha v4+ does not exit after tests.
